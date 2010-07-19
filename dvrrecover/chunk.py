ad93b05566bc662231f53f8220b37571e7d968b0
Stefan Haller <haliner@googlemail.com>
2010-07-19 17:09:31 +0200

    new chunk interface

--- dvrrecover/chunk.py
+++ dvrrecover/chunk.py
@@ -30,43 +30,69 @@ concat      = 'concat'
 
 class UnknownChunkKeyError(DvrRecoverError):
     """Invalid chunk key specified"""
-    pass
+
+    def __init__(self, key):
+        DvrRecoverError.__init__(self,
+            "Invalid chunk key specified: %s" % key)
 
 
-class ChunkNotAssociatedError(DvrRecoverError):
-    """Chunk not associated with database row"""
-    pass
+class ChunkAlreadyAssociatedError(DvrRecoverError):
+    """Chunk is already associated with database row"""
+
+    def __init__(self):
+        DvrRecoverError.__init__(self,
+            "Chunk is already associated with database row")
+
 
 
 class Chunk(object):
     """Object to save information about one chunk"""
-    __slots__ = ('id',)
-    
+    __slots__ = ('id', 'cache')
+
     def __init__(self, id=None):
         """Initalize Chunk object"""
         self.id = id
+        if not self.is_associated():
+            self.cache = {}
 
 
-    def is_valid_key(self, key):
+    def is_associated(self):
+        return self.id is not None
+
+
+    def is_valid_key(self, key, throw=False):
         """Return true if key is valid"""
-        return key in (block_start,
-                       block_size,
-                       clock_start,
-                       clock_end,
-                       concat)
+        value = key in (block_start,
+                        block_size,
+                        clock_start,
+                        clock_end,
+                        concat)
+        if not value and throw:
+            raise UnknownChunkKeyError(key)
 
 
     def get(self, key):
         """Return value of chunk info specified by key"""
-        if not self.is_valid_key(key):
-            raise UnknownChunkKeyError("No valid chunk key: %s" %
-                                        key)
-        return instances.db.chunk_query(key)
+        self.is_valid_key(key, True)
+        if self.is_associated():
+            return instances.db.chunk_query(self.id, key)
+        else:
+            return self.cache[key]
 
 
     def set(self, key, value):
         """Change chunk info and update database"""
-        if not self.is_valid_key(key):
-            raise UnknownChunkKeyError("No valid chunk key: %s" %
-                                        key)
-        instances.db.chunk_insert(key, value)
+        self.is_valid_key(key, True)
+        if self.is_associated():
+            instances.db.chunk_update(self.id, key, value)
+        else:
+            self.cache[key] = value
+
+
+    def save(self):
+        """Inserts chunk info into database"""
+        if self.is_associated():
+            raise ChunkAlreadyAssociatedError()
+        self.id = instances.db.chunk_insert()
+        for k, v in self.cache.iteritems():
+            self.set(k, v)
